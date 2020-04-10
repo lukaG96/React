95c70f1d1043499f7dde2c8498c6c044db341fc4
Luka <luka1996gacic@gmail.com>
2020-04-10 14:11:59 +0200

    setRouter

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import React from "react";
 import { Provider } from "react-redux";
 import Team from "./Components/teams/Team";
 import store from "./redux/store";
-import { Router } from "react-router-dom";
-import TeamDetails from "./Components/teams/TeamDetails";
 
 const App = () => {
   return (
     <div>
-      <Router>
-        <Provider store={store}>
-          <Team path="/" />
-          <TeamDetails path="/details/:id" />
-        </Provider>
-      </Router>
+      <Provider store={store}>
+        <Team />
+      </Provider>
     </div>
   );
 };
