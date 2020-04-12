2d7f0a840e3d60ec3bb30e20e6039cc5bb76f4da
Luka <luka1996gacic@gmail.com>
2020-04-12 15:39:39 +0200

    add team details

--- src/Components/teams/TeamDetails.js
+++ src/Components/teams/TeamDetails.js
@@ -1,15 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function TeamDetails(props) {
-  const [teamId, setteamId] = useState(null);
-  //const [team, setteam] = useState({});
+  const teamFromRedux = useSelector((state) =>
+    state.teamsKey.teams.find(
+      (x) => x.id === parseInt(props.match.params.team_id)
+    )
+  );
+  const [team, setTeam] = useState({});
+
+  function mapTeam() {
+    if (teamFromRedux) {
+      setTeam({
+        name: teamFromRedux.name,
+        abbreviation: teamFromRedux.abbreviation,
+        city: teamFromRedux.city,
+        conference: teamFromRedux.conference,
+        division: teamFromRedux.division,
+        full_name: teamFromRedux.full_name,
+      });
+    } else {
+      //redirect to error page
+    }
+  }
 
   useEffect(() => {
-    let id = props.match.params.team_id;
-    setteamId(id);
-  }, [props.match.params]);
-  //console.log(props);
-  return <div>{teamId}</div>;
+    mapTeam();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return team ? <div>{team.full_name}</div> : <div> no data </div>;
 }
 
 export default TeamDetails;
